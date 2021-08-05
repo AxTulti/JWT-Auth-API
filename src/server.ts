4944716b614d22ce66d431006d660f3d1c935290
Axel <axelyaelrodriguezromero@gmail.com>
2021-08-05 10:49:01 -0500

    Cleaned up the CORS related code, and updated the tar npm pakage to 6.1.6 t avoid a bug in previous versions.

--- src/server.ts
+++ src/server.ts
@@ -31,14 +31,16 @@ app.set("port", process.env.PORT || 3000);
 // midleware
 // Allow CORS
 const whitelist: string[] | any = process.env.ALLOWED_ORIGINS!;
+function manageOriginAcces(origin: string, callback: Function):void {
+    // if the origin is included in the whitelist call the callback
+    if (whitelist.includes(origin)) callback(null, true);
+    // otherwise, pass an error to the callback
+    else callback(new Error('Not allowed by CORS'));
+}
+
 const corsOptions: any = {
-    origin: function (origin: any, callback: any) {
-        if (whitelist.includes(origin)) {
-          callback(null, true)
-        } else {
-          callback(new Error('Not allowed by CORS'))
-        }
-    }}
+    origin: manageOriginAcces
+};
 app.use(cors(corsOptions));
 app.use(morgan('tiny'));
 
