11f0404dbfcbd15e267178d03deb9e9572c95031
Axel <axelyaelrodriguezromero@gmail.com>
2021-08-18 12:17:47 -0500

    This time I hope the CORS is working.

--- src/server.ts
+++ src/server.ts
@@ -32,15 +32,15 @@ app.set("port", process.env.PORT || 3000);
 // Allow CORS
 const whitelist: string[] | any = process.env.ALLOWED_ORIGINS!;
 
-// function manageOriginAcces(origin: string, callback: Function):void {
-//     // if the origin is included in the whitelist call the callback
-//     if (whitelist.includes(origin)) callback(null, true);
-//     // otherwise, pass an error to the callback
-//     else callback(new Error('Not allowed by CORS'));
-// }
+function manageOriginAcces(origin: string, callback: Function):void {
+    // if the origin is included in the whitelist call the callback
+    if (whitelist.includes(origin)) callback(null, true);
+    // otherwise, pass an error to the callback
+    else callback(new Error('Not allowed by CORS'));
+}
 
 const corsOptions: any = {
-    origin: whitelist
+    origin: manageOriginAcces
 };
 
 app.use(cors(corsOptions));
